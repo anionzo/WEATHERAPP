32a99e1685516735334ee31d5bf908538e1546bc
mimieu-admin <tienyoyoyo@gmail.com>
2023-04-24 01:24:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click',()=>{
+
+    const APIKey = '341d6d0346c11ac403dd627762a9b468';
+    const city = document.querySelector('.search-box  input').value;
+
+    if(city === '')
+        return;
+    fetch('')
+
+})
