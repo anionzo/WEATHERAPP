52bafdc325d7f61341906b01f5f162c674630426
Mi Miêu <86876887+anionzo@users.noreply.github.com>
2023-04-24 07:37:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ search.addEventListener('click', () => {
 
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
             description.innerHTML = `${json.weather[0].description}`;
-            humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+            humidity.innerHTML = `${json.main.humidity} %`;
+            wind.innerHTML = `${parseInt(json.wind.speed)} Km/h`;
 
             weatherBox.style.display = '';
             weatherDetails.style.display = '';
